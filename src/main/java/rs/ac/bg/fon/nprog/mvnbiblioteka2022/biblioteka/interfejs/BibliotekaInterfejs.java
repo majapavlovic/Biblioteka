7617bc2d66e0cc0722fca46dc3750a7a8723d563
majapavlovic <maja.pavlovic2703@gmail.com>
2022-03-30 12:53:41 +0200

    unete ispravke za javadoc

--- src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2022/biblioteka/interfejs/BibliotekaInterfejs.java
+++ src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2022/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -23,7 +23,7 @@ public interface BibliotekaInterfejs {
 	 * @throws java.lang.IllegalArgumentException ako uneta knjiga vec postoji u biblioteci
 	 * 
 	 * @see <a href = "https://github.com">GutHub sajt</a>
-	 * @see biblioteka.Knjiga
+	 * @see  rs.ac.bg.fon.nprog.mvnbiblioteka2022.biblioteka.Knjiga
 	 */
 	public void dodajKnjigu(Knjiga knjiga);
 	
